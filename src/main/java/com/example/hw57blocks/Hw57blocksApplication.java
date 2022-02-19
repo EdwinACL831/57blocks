2a4654a8fe22f913a980b75e3f6feb76231183f9
edwin.collazos <edwin.collazos@geniussports.com>
2022-02-19 12:04:12 -0500

    57block: Creates Spring Boot project

---
+++ src/main/java/com/example/hw57blocks/Hw57blocksApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw57blocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw57blocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw57blocksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hw57blocks/Hw57blocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw57blocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw57blocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
